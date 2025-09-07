0eadebd0727c8cc6697f9f7afdc4b654e439fc7d
v1r4m <choyoung7789@gmail.com>
2025-09-07 14:46:38 +0900

    dockerfile

--- app.py
+++ app.py
@@ -207,4 +207,5 @@ def stream_track(track_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
--- dockerfile
+++ dockerfile
@@ -6,4 +6,4 @@ WORKDIR /app
 
 RUN pip3 install -r requirements.txt
 
-CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0", "--port=8080"]
+CMD ["python3", "app.py"]
